0f37ba46cb9c26df894ae527fa2cc9c10a97d15b
VinayKumar-MK <datasourcethbs@gmail.com>
2021-08-12 19:30:44 +0530

    Full application Commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 //import logo from './logo.svg';
 import './App.css';
-import Expenses from './component/Expenses';
+import Expenses from './component/Expenses/Expenses';
+import NewExpense from './component/NewExpense/NewExpense';
+import React,{useState} from 'react';
 //import ExpenseItem from './component/ExpenseItem';
 //import {usePreventScreenCapture} from 'expo-screen-capture';
 //import EventEmitter from 'EventEmitter';
 
-function App() {
-
-  const expenses
-   = [
+const DUMMY_EXPENSES 
+= [
+{
+   id: 'e1',
+   title: 'Toilet Paper',
+   amount: 94.12,
+   date: new Date(2020, 7, 14),
+ },
+ { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
  {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+   id: 'e3',
+   title: 'Car Insurance',
+   amount: 294.67,
+   date: new Date(2021, 2, 28),
 
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+ },
+ {
+   id: 'e4',
+   title: 'New Desk (Wooden)',
+   amount: 450,
+   date: new Date(2021, 5, 12),
+ },
+];
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
+        <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
     </div>
   );
---
+++ src/component/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+//import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from '../UI/Card';
+import ExpensesChart from './ExpensesChart';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import React,{useState} from 'react';
+
+const Expenses = (props)  => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+
+    return (
+        <div className="expenses">
+            <Card className='expenses'>
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </div>
+
+    );
+};
+
+
+export default Expenses;
